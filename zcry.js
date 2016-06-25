db15627960201c223831130041012c984e18883b
Clark-Zhao <553867163@qq.com>
2016-06-25 21:55:02 +0800

    start

---
+++ zcry.js
@@ -0,0 +1,21 @@
+/*!
+ * zCry
+ * Copyright 2016 Clark Zhao (zhaoyuxiang.cn)
+ * Licensed under the MIT License:
+ *   http://www.opensource.org/licenses/mit-license.php
+ */
+
+// Global undefined variable
+window.undefined = window.undefined;
+
+/**
+ * Create a new zCry Object
+ * @constructor
+ */
+function zCry(){
+
+}
+
+zCry.fn = zCry.prototype = {
+
+}
