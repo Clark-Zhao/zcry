a199269d72f2b56da0146382c129c7f592d1f297
Clark-Zhao <553867163@qq.com>
2016-06-28 17:22:43 +0800

    事件监听兼容性测试

--- zcry.js
+++ zcry.js
@@ -12,10 +12,51 @@ window.undefined = window.undefined;
  * Create a new zCry Object
  * @constructor
  */
-function zCry(){
-
+zCry = function(){
+	
 }
+
 
 zCry.fn = zCry.prototype = {
 
+	addHandler: function(element,type,handler){
+		if (element.addEventListener) {
+			element.addEventListener(type,handler,false);
+		}else if (element.attachEvent) {
+			element.attachEvent("on"+type,handler);
+		}else {
+			element["on"+type] = handler;
+		}
+	},
+	removeHandler: function(element,type,handler){
+		if (element.removeEventListener) {
+			element.removeEventListener(type,handler,false);
+		}else if (element.detachEvent) {
+			element.detachEvent("on"+type,handler);
+		}else {
+			element["on"+type] = null;
+		}
+	},
+	getEvent: function(event){
+		return event ? event : window.event;
+	},
+	getTarget: function(event){
+		return event.target || event.srcElement;
+	},
+	preventDefault: function(event){
+		if (event.preventDefault()) {
+			event.preventDefault();
+		}else{
+			event.returnValue = false;
+		}
+	},
+	stopPropagation: function(event){
+		if (event.stopPropagation()) {
+			event.stopPropagation();
+		}else{
+			event.cancleBubble = true;
+		}
+	}
 }
+
+window["zCry"] = zCry;
